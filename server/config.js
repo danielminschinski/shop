8cffb9669ac9dd4681e6f20db875da7cc0b65fa0
Daniel <danielminschinski@gmail.com>
2020-03-05 13:12:52 +0100

    modified project

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
-    DB: 'mongodb+srv://agat:Perseverance92@cluster0-kvp2j.mongodb.net/test?retryWrites=true&w=majority',
+    DB: 'mongodb+srv://blackmin:michnik1990@cluster0-49iqr.gcp.mongodb.net/test?retryWrites=true&w=majority',
 };
